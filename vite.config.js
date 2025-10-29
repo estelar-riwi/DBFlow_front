7224464b36152f9d40b4a674bc0b73b9587800fb
jrestrepo18 <jerorrpo@gmail.com>
2025-10-28 21:10:34 -0500

    FEAT: Inicialización el proyecto

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path' // <-- 1. IMPORTA ESTO
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 2. AÑADE ESTE BLOQUE COMPLETO
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
